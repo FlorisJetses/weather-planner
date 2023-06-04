d3d5f57356a1907849da384db34b60d6991e735c
Floris <florisjoepie@gmail.com>
2023-06-04 23:51:23 +0200

    feat: add base colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    colors: {
+      purple: "#5115F7",
+      rose: "#F0EBFE",
+      gray100: "#E9E9E9",
+      gray200: "#D9D9D9",
+      gray300: "#C4C4C4",
+      gray400: "#9D9D9D",
+      white: "#F5F5F5",
+      black: "#000000"
+
+    },
     extend: {},
   },
   plugins: [],
